07e6547bae3972a8dd2e32128f73809cf5cd17c8
Mohammad Bijantium Sinatria <79687063+Bintaaaa@users.noreply.github.com>
2022-07-16 01:15:32 +0700

    usecase auth

---
+++ domains/authentication/lib/domain/usecases/sign_up_usecase.dart
@@ -0,0 +1,19 @@
+import 'package:authentication/domain/entity/body/auth_request_entity.dart';
+import 'package:authentication/domain/entity/response/auth_response_entity.dart';
+import 'package:authentication/domain/repository/authentication_repository.dart';
+import 'package:common/utils/error/failure_response.dart';
+import 'package:common/utils/use_case/use_case.dart';
+import 'package:dartz/dartz.dart';
+
+class SignUpUseCase extends UseCase<AuthResponseEntity, AuthRequestEntity> {
+  final AuthenticationRepository authenticationRepository;
+  SignUpUseCase({required this.authenticationRepository});
+
+  @override
+  Future<Either<FailureResponse, AuthResponseEntity>> call(
+    AuthRequestEntity params,
+  ) async =>
+      await authenticationRepository.sighnUp(
+        authRequestEntity: params,
+      );
+}
