cfea6c55f465eada07dee04fc1fdff2394b850a2
Mohammad Bijantium Sinatria <79687063+Bintaaaa@users.noreply.github.com>
2022-07-16 01:15:44 +0700

    Repository Impl

--- domains/authentication/lib/data/repository/authentication_repository_impl.dart
+++ domains/authentication/lib/data/repository/authentication_repository_impl.dart
@@ -1,13 +1,22 @@
 import 'package:authentication/data/datasource/local/authentication_local_datasource.dart';
+import 'package:authentication/data/datasource/remote/authentication_remote_datasource.dart';
+import 'package:authentication/data/mapper/authentication_mapper.dart';
+import 'package:authentication/domain/entity/response/auth_response_entity.dart';
+import 'package:authentication/domain/entity/body/auth_request_entity.dart';
 import 'package:authentication/domain/repository/authentication_repository.dart';
+import 'package:common/utils/constants/app_constants.dart';
 import 'package:common/utils/error/failure_response.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dependencies/dio/dio.dart';
 
 class AuthenticationRepositoryImpl implements AuthenticationRepository {
   final AuthenticationLocalDataSources authenticationLocalDataSources;
-
+  final AuthenticationRemoteDataSource authenticationRemoteDataSource;
+  final AuthenticationMapper authenticationMapper;
   AuthenticationRepositoryImpl({
     required this.authenticationLocalDataSources,
+    required this.authenticationRemoteDataSource,
+    required this.authenticationMapper,
   });
 
   @override
@@ -28,6 +37,48 @@ class AuthenticationRepositoryImpl implements AuthenticationRepository {
       return Right(response);
     } on Exception catch (error) {
       return Left(FailureResponse(errorMessage: error.toString()));
+    }
+  }
+
+  @override
+  Future<Either<FailureResponse, AuthResponseEntity>> sighnIn(
+      {required AuthRequestEntity authRequestEntity}) async {
+    try {
+      final response = await authenticationRemoteDataSource.signIn(
+        authRequestDto: authenticationMapper
+            .mapAuthRequestEntityToAuthRequestDto(authRequestEntity),
+      );
+      return Right(
+        authenticationMapper.mapAuthResponseDtoToAuthResponseEntity(response),
+      );
+    } on DioError catch (e) {
+      return Left(
+        FailureResponse(
+          errorMessage:
+              e.response?.data[AppConstants.errorKey.message.toString()],
+        ),
+      );
+    }
+  }
+
+  @override
+  Future<Either<FailureResponse, AuthResponseEntity>> sighnUp(
+      {required AuthRequestEntity authRequestEntity}) async {
+    try {
+      final response = await authenticationRemoteDataSource.signUp(
+        authRequestDto: authenticationMapper
+            .mapAuthRequestEntityToAuthRequestDto(authRequestEntity),
+      );
+      return Right(
+        authenticationMapper.mapAuthResponseDtoToAuthResponseEntity(response),
+      );
+    } on DioError catch (e) {
+      return Left(
+        FailureResponse(
+          errorMessage:
+              e.response?.data[AppConstants.errorKey.message.toString()],
+        ),
+      );
     }
   }
 }
